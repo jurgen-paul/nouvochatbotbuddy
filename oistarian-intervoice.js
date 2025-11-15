c6a4dbf684e1dc699bc221ad0a980a657a6dd17d
Joachim paul westerveld <126770103+jurgen-paul@users.noreply.github.com>
2025-11-15 09:12:05 +0100

    Create oistarian-intervoice.js

---
+++ oistarian-intervoice.js
@@ -0,0 +1,10 @@
+{
+  "$schema": "https://openapi.vercel.sh/microfrontends.json",
+  "applications": {
+    "oistarians-empathic-voice-interface-starter": {
+      "development": {
+        "fallback": "oistarians-empathic-voice-interface.vercel.app"
+      }
+    }
+  }
+}
